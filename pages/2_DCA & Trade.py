4d743592069010408442f7ef9f0db96b44c7f25b
phawitb <96680592+phawitb@users.noreply.github.com>
2025-01-19 01:52:56 +0700

    Update 2_DCA & Trade.py

--- pages/2_DCA & Trade.py
+++ pages/2_DCA & Trade.py
@@ -107,123 +107,110 @@ def get_all_trade_history(API_KEY,API_SECRET,symbol, start=None, end=None, limit
     return all_trades
 
 st.write("# DCA & Trade! 👋")
+if person_id:
+    API_URL = configures.api_url 
+    params = {
+        "EMAIL": person_id
+    }
 
-# st.write(get_server_time())
-# server_time = get_server_time()
-# converted_datetime = datetime.fromtimestamp(server_time/1000)
-# st.write(converted_datetime)
-
-API_URL = configures.api_url 
-params = {
-    "EMAIL": person_id
-}
-
-if not st.session_state.person_data:
-    with st.spinner('Waiting'):
-        response = requests.get(API_URL, params=params)
-        if response.status_code == 200:
-            st.session_state.person_data = response.json()
-        else:
-            st.session_state.person_data = None
-
-if st.session_state.person_data['status'] == 'error':
-    API_KEY = ""
-    API_SECRET = ""
-    DCA_AMOUNT = 0
-    TRADES = [[0,0,0],[0,0,0],[0,0,0]]
-else:
-    API_KEY = st.session_state.person_data['data']['API_KEY']
-    API_SECRET = st.session_state.person_data['data']['API_SECRET']
-    API_SECRET = st.session_state.person_data['data']['API_SECRET']
-    DCA_AMOUNT = st.session_state.person_data['data']['DCA_AMOUNT']
-    TRADE1 = st.session_state.person_data['data']['TRADE1']
-    TRADE2 = st.session_state.person_data['data']['TRADE2']
-    TRADE3 = st.session_state.person_data['data']['TRADE3']
-
-    TRADES = [TRADE1,TRADE2,TRADE3]
-
-    for i in range(len(TRADES)):
-        if TRADES[i]:
-            TRADES[i] = ast.literal_eval(TRADES[i])
-        else:
-            TRADES[i] = [0,0,0]
-
-LIST_MENUS  = ['Setup','DCA','Trade']
-tab1,tab2,tab3 = st.tabs(LIST_MENUS)
-    
-api_key = tab1.text_input("API_KEY :",API_KEY)
-api_secret = tab1.text_input("API_SECRET :", API_SECRET,type="password")
-
-if tab1.button("Update"):
-    with st.spinner('Waiting'):
-        payload = {
-            "EMAIL": person_id,
-            "API_KEY": api_key,
-            "API_SECRET": api_secret,
-        }
-        response = requests.post(API_URL, json=payload)
-
-        tab1.write('update complete!')
-
-dca_amount = tab2.number_input("DCA_AMOUNT :",value=DCA_AMOUNT,min_value=0,max_value=None,key=f"dca")
-
-if tab2.button("Update!"):
-    with st.spinner('Waiting'):
-        payload = {
-            "EMAIL": person_id,
-            "DCA_AMOUNT": dca_amount
-        }
-        response = requests.post(API_URL, json=payload)
-        tab2.write('update complete!')
-
-tab2.write("## Historys")
-all_trades = get_all_trade_history(API_KEY,API_SECRET,"btc_thb")
-if all_trades:
-    # st.write(all_trades)
-    df_historys = pd.DataFrame(all_trades)
-
-    # Adjust `ts` with server time
-    if 'ts' in df_historys.columns:
-        server_time = get_server_time()
-        df_historys['adjusted_ts'] = df_historys['ts'] - (server_time - int(time.time() * 1000))
-        df_historys['datetime'] = pd.to_datetime(df_historys['adjusted_ts'], unit='ms')
-        df_historys['datetime'] = df_historys['datetime'] + timedelta(hours=7)
-
-    df_history_dca = df_historys[df_historys['client_id'] == 'dca']
-    df_history_dca.set_index('datetime', inplace=True)
-    df_history_dca = df_history_dca[['side', 'type', 'rate', 'fee', 'amount']]
-    df_history_dca['rate'] = df_history_dca['rate'].astype(float)
-    df_history_dca['rate'] = df_history_dca['rate'].apply(lambda x: f"{x:,.2f}")
-    tab2.write(df_history_dca)
-else:
-    tab2.write("No trade history available.")
-
-
-#tab3 Trade
-cols = tab3.tabs(['Bot1','Bot2','Bot3'])
-for i in range(len(cols)):
-    trade_profit = cols[i].number_input ("trade1_profit :",value=TRADES[i][0],min_value=0,max_value=100,key=f"u{i}")
-    trade_cutloss = cols[i].number_input("trade1_cutloss :",value=TRADES[i][1],min_value=0,max_value=100,key=f"l{i}")
-    trade_amount = cols[i].number_input("trade1_amount :",value=TRADES[i][2],min_value=0,max_value=None,key=f"a{i}")
-
-    if cols[i].button(f"Update", key=f"bot{i}"):
+    if not st.session_state.person_data:
         with st.spinner('Waiting'):
-            TRADES[i] = [trade_profit,trade_cutloss,trade_amount]
+            response = requests.get(API_URL, params=params)
+            if response.status_code == 200:
+                st.session_state.person_data = response.json()
+            else:
+                st.session_state.person_data = None
+
+    if st.session_state.person_data['status'] == 'error':
+        API_KEY = ""
+        API_SECRET = ""
+        DCA_AMOUNT = 0
+        TRADES = [[0,0,0],[0,0,0],[0,0,0]]
+    else:
+        API_KEY = st.session_state.person_data['data']['API_KEY']
+        API_SECRET = st.session_state.person_data['data']['API_SECRET']
+        API_SECRET = st.session_state.person_data['data']['API_SECRET']
+        DCA_AMOUNT = st.session_state.person_data['data']['DCA_AMOUNT']
+        TRADE1 = st.session_state.person_data['data']['TRADE1']
+        TRADE2 = st.session_state.person_data['data']['TRADE2']
+        TRADE3 = st.session_state.person_data['data']['TRADE3']
+
+        TRADES = [TRADE1,TRADE2,TRADE3]
+
+        for i in range(len(TRADES)):
+            if TRADES[i]:
+                TRADES[i] = ast.literal_eval(TRADES[i])
+            else:
+                TRADES[i] = [0,0,0]
+
+    LIST_MENUS  = ['Setup','DCA','Trade']
+    tab1,tab2,tab3 = st.tabs(LIST_MENUS)
+        
+    api_key = tab1.text_input("API_KEY :",API_KEY)
+    api_secret = tab1.text_input("API_SECRET :", API_SECRET,type="password")
+
+    if tab1.button("Update"):
+        with st.spinner('Waiting'):
             payload = {
                 "EMAIL": person_id,
-                f"TRADE{i+1}": str(TRADES[i])
+                "API_KEY": api_key,
+                "API_SECRET": api_secret,
             }
-    
             response = requests.post(API_URL, json=payload)
 
-            cols[i].write('update complete!')
+            tab1.write('update complete!')
+
+    dca_amount = tab2.number_input("DCA_AMOUNT :",value=DCA_AMOUNT,min_value=0,max_value=None,key=f"dca")
+
+    if tab2.button("Update!"):
+        with st.spinner('Waiting'):
+            payload = {
+                "EMAIL": person_id,
+                "DCA_AMOUNT": dca_amount
+            }
+            response = requests.post(API_URL, json=payload)
+            tab2.write('update complete!')
+
+    tab2.write("## Historys")
+    all_trades = get_all_trade_history(API_KEY,API_SECRET,"btc_thb")
+    if all_trades:
+        # st.write(all_trades)
+        df_historys = pd.DataFrame(all_trades)
+
+        # Adjust `ts` with server time
+        if 'ts' in df_historys.columns:
+            server_time = get_server_time()
+            df_historys['adjusted_ts'] = df_historys['ts'] - (server_time - int(time.time() * 1000))
+            df_historys['datetime'] = pd.to_datetime(df_historys['adjusted_ts'], unit='ms')
+            df_historys['datetime'] = df_historys['datetime'] + timedelta(hours=7)
+
+        df_history_dca = df_historys[df_historys['client_id'] == 'dca']
+        df_history_dca.set_index('datetime', inplace=True)
+        df_history_dca = df_history_dca[['side', 'type', 'rate', 'fee', 'amount']]
+        df_history_dca['rate'] = df_history_dca['rate'].astype(float)
+        df_history_dca['rate'] = df_history_dca['rate'].apply(lambda x: f"{x:,.2f}")
+        tab2.write(df_history_dca)
+    else:
+        tab2.write("No trade history available.")
 
 
+    #tab3 Trade
+    cols = tab3.tabs(['Bot1','Bot2','Bot3'])
+    for i in range(len(cols)):
+        trade_profit = cols[i].number_input ("trade1_profit :",value=TRADES[i][0],min_value=0,max_value=100,key=f"u{i}")
+        trade_cutloss = cols[i].number_input("trade1_cutloss :",value=TRADES[i][1],min_value=0,max_value=100,key=f"l{i}")
+        trade_amount = cols[i].number_input("trade1_amount :",value=TRADES[i][2],min_value=0,max_value=None,key=f"a{i}")
 
-# st.write(all_trades)
-# df_historys = pd.DataFrame(all_trades)
-# df_history_dca = df_historys[df_historys['client_id'] == 'dca']
-# df_history_dca['datetime'] = pd.to_datetime(df_history_dca['ts'], unit='ms')  # Specify unit='s' for seconds
+        if cols[i].button(f"Update", key=f"bot{i}"):
+            with st.spinner('Waiting'):
+                TRADES[i] = [trade_profit,trade_cutloss,trade_amount]
+                payload = {
+                    "EMAIL": person_id,
+                    f"TRADE{i+1}": str(TRADES[i])
+                }
+        
+                response = requests.post(API_URL, json=payload)
 
-
-# st.write(df_history_dca)
+                cols[i].write('update complete!')
+else:
+    st.write('### Please login!!!')
